fd8e76e8febd57b15eece7f61a91f316a4edf173
Lucas Marques de Faria <90582583+lucasmarquesfaria@users.noreply.github.com>
2024-06-06 08:15:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 
+$name = 'peson';
+
+$person ['name' => 'Lucas', 'age' => '25', 
+        'documents' => ['cpf' => '11938597855', 'cit' => 'caratinga', 'state' => 'mg',
+        'array' => ['dadoos ok', 'dados faltando'];
+
+var_dump($person['name']);
