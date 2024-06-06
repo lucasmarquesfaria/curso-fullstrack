f44d2859fbb81bd74cd9a72088e798868c49cd10
Lucas Marques <marquesfaria99@gmail.com>
2024-06-05 21:43:16 -0300

    novos arquivos

--- varAmbiente.php
+++ varAmbiente.php
@@ -2,8 +2,9 @@
 
 require './vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+$dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
 
-var_dump($_ENV['KEY']);
+var_dump(['DATABASE']);
 
