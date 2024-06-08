f7445fd358d49d6fd9c2a75460d48d8935a583a4
Lucas Marques <marquesfaria99@gmail.com>
2024-06-07 21:09:51 -0300

    modified:   index.php 	modified:   index.php

--- index.php
+++ index.php
@@ -2,10 +2,34 @@
 
 mb_http_output('UTF-8');
 
-$name = 'peson';
+$name = 'person';
+$title = 'Codigos PHP';
 
-$person ['name' => 'Lucas', 'age' => '25', 
-        'documents' => ['cpf' => '11938597855', 'cit' => 'caratinga', 'state' => 'mg',
-        'array' => ['dadoos ok', 'dados faltando'];
 
-var_dump($person['name']);
+$person = [
+        'name' => 'Lucas', 
+        'age' => '25',      
+        'documents' => [
+                'cpf' => '11938597855', 
+                'cit' => 'caratinga', 
+                'state' => 'mg',
+                'array' => ['dadoos ok', 'dados faltando']
+                ]
+];
+
+
+var_dump($person['age']);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $title?></title>
+</head>
+<body>
+        
+</body>
+</html>
