925d6f23428b2537ddcb3e507f76de67fd6fb170
Lucas Marques <marquesfaria99@gmail.com>
2024-06-07 21:11:04 -0300

    modified:   index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ var_dump($person['age']);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
